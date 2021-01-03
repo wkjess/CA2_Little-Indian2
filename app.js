6bb06a62ae5c4e8d2cd16bcefdbd2f60e188c574
Jessie Wong <kimjang.wong1@gmail.com>
2021-01-03 13:10:54 +0000

    spelling check

--- app.js
+++ app.js
@@ -166,6 +166,6 @@ function lookupEntity(name) {
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
 
     var addr = server.address();
-    console.log("Server listnening at", addr.address + ":" + addr.port);
+    console.log("Server listening at", addr.address + ":" + addr.port);
 
 });
