2ef4157f2d004644a3f2b14e008d8cdabf63c01d
Jessie Wong <kimjang.wong1@gmail.com>
2021-03-29 12:07:33 +0000

    created routes & user-controller

---
+++ routes.js
@@ -0,0 +1,16 @@
+const express = require('express'),
+router = express.Router();
+
+var itemCtrl = require('./item-controller'),
+userCtrl = require('./user-controller');
+
+router.get('/hello', itemCtrl.getWorld);
+
+router.get('/hello/:foo/:bar', itemCtrl.getWorldParams);
+
+router.post('/hello', itemCtrl.postWorld);
+
+router.post('/users', userCtrl.createUser);
+router.get('/users', userCtrl.getUsers);
+
+module.exports = router;
