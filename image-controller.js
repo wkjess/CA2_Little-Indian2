60e76e46fafc3d77030b67498d6440a52b484248
Jessie Wong <kimjang.wong1@gmail.com>
2021-04-09 12:12:08 +0000

    created image-controller.js

---
+++ image-controller.js
@@ -0,0 +1,37 @@
+const UPLOAD_PATH = require('./routes').UPLOAD_PATH,
+Image = require('./models/image'),
+path = require('path'),
+fs = require('fs'),
+del = require('del');
+
+exports.uploadImage = function(req, res)
+{
+    let newImage = new Image();
+    newImage.filename = req.file.filename;
+    newImage.originalName = req.file.originalname;
+    newImage.desc = req.body.desc;
+    newImage.save(err => {
+        if (err){
+            return res.sendStatus(400);
+        }
+        res.status(201).send({ newImage })
+    });
+
+};
+
+exports.getImages = function(req, res) {
+    Image.find({}, '-__v')
+    .lean()
+    .exec((err, images) => {
+        if (err) {
+            return res.sendStatus(400);
+        }
+
+        for (let i = 0; i < images.length; i++) {
+            var img = images[i];
+            img.url = req.protocol + '://' + req.get('host') + '/images/' + img._id;
+        }
+
+        res.json(images);
+    });
+};
